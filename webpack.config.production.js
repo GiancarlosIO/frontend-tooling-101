a292189d7487fbbe61b0135b3dcfee90213fa6b1
Giancarlos <giancarlos.isasi@gmail.com>
2019-10-31 02:27:29 -0500

    Build

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "/frontend-tooling-101/"
   },
   plugins: [
     new webpack.DefinePlugin({
